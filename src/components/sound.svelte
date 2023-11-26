97a5d43e2d97d7091cbd60fb590e076d1bcd4f84
Chris Schalago <chrisschalago@gmail.com>
2023-11-26 00:52:59 -0500

    Add loading and playing of sounds

---
+++ src/components/sound.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { sound } from "svelte-sound";
+    export let src;
+    let trimmedSrc = src.replace('/static', '');
+  </script>
+  
+  <button use:sound={{src: trimmedSrc, events: ["click"]}}>
+      {trimmedSrc}
+  </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    import Sound from '../components/Sound.svelte';
+    export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each data.sounds as sound}
+    <Sound src={sound} />
+{/each}
