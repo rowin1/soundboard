225be5e21690c735ff31b47de38dac578c044008
Chris Schalago <chrisschalago@gmail.com>
2023-11-26 08:30:06 -0500

    Change to client side loading for sounds.json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,24 @@
 <script>
+    import { onMount } from 'svelte';
     import Sound from '../components/Sound.svelte';
-    export let data;
+
+    /** @type {any} */
+    let subDirectories = [];
+
+    onMount(() => {
+        fetch('/sounds.json')
+        .then(response => response.json())
+        .then(data => {
+            subDirectories = data;
+        });
+    });
 </script>
 
 <div style="display: flex; justify-content: center;">
     <img src="pops.png" style="width: 100%; height: auto; object-fit: contain;" alt="pops face" />
 </div>
 
-{#each data.subDirectories as { category, files }}
+{#each subDirectories as { category, files }}
     <span>{category}</span>
     {#each files as { title, src}, soundIndex}
         <Sound {title} {src} {soundIndex} />
